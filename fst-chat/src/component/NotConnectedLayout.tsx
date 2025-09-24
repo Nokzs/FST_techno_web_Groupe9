4197371f70d58cd9871b51bc9beada38f977268f
nokzs <alexischapusot@gmail.com>
2025-09-24 21:44:28 +0200

    mise en place react Router mode data todo: hook authContext

---
+++ fst-chat/src/component/NotConnectedLayout.tsx
@@ -0,0 +1,6 @@
+import { Outlet } from "react-router-dom";
+
+export function NotConnectedLayout() {
+  //si connecter renvoyer au chat
+  return <Outlet />;
+}
--- fst-chat/src/main.tsx
+++ fst-chat/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { App } from "./App";
+import { NotConnectedLayout } from "./component/NotConnectedLayout";
+const router = createBrowserRouter([
+  {
+    Component: NotConnectedLayout,
+    children: [
+      {
+        path: "/",
+        Component: App,
+      },
+      {
+        path: "/login",
+        Component: App,
+      },
+      {
+        path: "/register",
+        Component: App,
+      },
+    ],
+  },
+]);
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);

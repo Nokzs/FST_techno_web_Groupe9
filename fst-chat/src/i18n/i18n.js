4cf826fb3221b459b1e2f0bc65076e33647c66c7
nokzs <alexischapusot@gmail.com>
2025-09-27 17:20:27 +0200

    ajout viteConfig

--- fst-chat/src/i18n/i18n.js
+++ fst-chat/src/i18n/i18n.js
@@ -22,7 +22,7 @@ i18n
   .init({
     fallbackLng: "en",
     debug: true,
-
+    load: "languageOnly",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },

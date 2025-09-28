e3ef824a6d29f68e265ef2a483012408c23b6c42
nokzs <alexischapusot@gmail.com>
2025-09-29 01:05:27 +0200

    amélioration responsive homePage

--- fst-chat/src/component/NotConnectedLayout.tsx
+++ fst-chat/src/component/NotConnectedLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
 export function NotConnectedLayout() {
   return (
-    <>
+    <div className="overflow-x-hidden">
       <NavBar />
       <Outlet />
-    </>
+    </div>
   );
 }
--- fst-chat/src/i18n/i18n.js
+++ fst-chat/src/i18n/i18n.js
@@ -20,11 +20,13 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en",
+    fallbackLng: ["fr"],
     debug: true,
-    load: "languageOnly",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
   });
 console.log("initialisation");

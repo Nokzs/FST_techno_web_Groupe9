2a8708d646b368820262c85832e246a1d89ca9f8
perez112u <coganperez@gmail.com>
2025-10-01 10:07:01 +0200

    correction du test message.service

--- fst-chat-back/src/message/service/message.service.spec.ts
+++ fst-chat-back/src/message/service/message.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessageService } from './message.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('MessageService', () => {
   let service: MessageService;
 
+  const mockMessageModel = {
+    create: jest.fn(),
+    find: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+    exec: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessageService],
+      providers: [
+        MessageService,
+        {
+          provide: getModelToken('Message'), 
+          useValue: mockMessageModel,      
+        },
+      ],
     }).compile();
 
     service = module.get<MessageService>(MessageService);

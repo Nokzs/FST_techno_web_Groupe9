7f7764970da023d016c0fb466ab77feeb86c15e0
Alexis chapusot <77673271+Nokzs@users.noreply.github.com>
2025-10-14 19:53:34 +0200

    Fix import path for TokenService in authGuard

--- fst-chat-back/src/guards/authGuard.ts
+++ fst-chat-back/src/guards/authGuard.ts
@@ -5,7 +5,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { TokenService } from 'src/token/token.service';
+import { TokenService } from '../token/token.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private tokenService: TokenService) {}

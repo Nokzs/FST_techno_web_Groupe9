795e66c7eb77429109c60ab6f1c2f0dd1f10b093
yahyabenbaba <yahyabenbaba1@gmail.com>
2025-11-01 17:57:03 +0100

    test(auth): aligne notAuthMiddleware.test sur redirection /servers

--- fst-chat-back/src/server/controller/server.controller.ts
+++ fst-chat-back/src/server/controller/server.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Req, UseGuards, UnauthorizedException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ServerService } from '../service/server.service';
 import { plainToInstance } from 'class-transformer';
 import { ServerDto } from '../DTO/server.dto';
